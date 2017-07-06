9d40a4b7084d2cc57d3c472ef7cbe8e6ed4fc62d
rover <tdunn1248@gmail.com>
2017-07-06 14:57:41 -0700

    imdb http get function

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+
+-[ ] use Node.js's http module to make a request to imdb.com
+-[ ] use the cheerio library to extract contents from the HTML page.
+-[ ] functions are tested with mocha and chai
---
+++ movie-search-cli.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const cheerio = require('cheerio')
+
+function movieSearch(query) {
+  http.get(
+    `http://www.imdb.com/find?ref_=nv_sr_fn&q=${query}&s=all`,
+    (res) => {
+    var html = ''
+    res.on('data', (chunk) => { html += chunk})
+    res.on('end', () => {
+      const $ = cheerio.load(html)
+      const movieTitles = $('.findSection').first().find('.result_text')
+        .map((i, elm) =>
+        $(elm).text())
+        .toArray()
+      console.log(movieTitles.join('\n'))
+      return movieTitles
+    })
+  })
+}
+
+movieSearch(process.argv[2])
